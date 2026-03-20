4cb6b91a141aa23ecfd78f0dbd8354f1cd16af03
shad0w0P <atulrajrj172001@gmail.com>
2026-03-21 00:56:43 +0530

    fix CORS origin

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="HRMS Lite API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hrms-atul.netlify.app/"],
+    allow_origins=["https://hrms-atul.netlify.app",  "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
